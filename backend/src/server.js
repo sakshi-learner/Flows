73a2f568f201c796f5346f07f0be182af6b38920
sakshi-learner <144124963+sakshi-learner@users.noreply.github.com>
2026-03-16 22:08:21 +0530

    add webhook discord server for log

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,12 @@ const app = require("./app");
 const { sequelize } = require("./models");
 const startConsumer = require("./kafka/consumer");
 const { initSocket } = require("../config/socket.config");
-
+const axios = require("axios");
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+const WEBHOOK_URL =process.env.Discord_WEBHOOK_URL;
 
 (async () => {
   try {
@@ -21,8 +23,10 @@ const server = http.createServer(app);
     server.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
+    await axios.post(WEBHOOK_URL, { content: "server runing" })
 
   } catch (err) {
     console.error("❌ Server startup failed:", err);
+    await axios.post(WEBHOOK_URL, { content: "server runing",err })
   }
 })();
